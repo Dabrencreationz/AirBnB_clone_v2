c40ae4dba4f50611f4ef9982a170bf7d9be62afe
Mowah Chibuike <mctimmykings@gmail.com>
2023-03-21 20:34:07 +0000

    update the Place class

--- models/place.py
+++ models/place.py
@@ -3,6 +3,7 @@
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, Integer, Float, Table, ForeignKey
 from sqlalchemy.orm import relationship
+from models.review import Review
 
 class Place(BaseModel, Base):
     """Representation of Place """
