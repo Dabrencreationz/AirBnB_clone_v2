15ecbf90703ca62678d2946369e1d3f97650797d
Brenda <brenvaltessy@yahoo.com>
2023-03-24 02:22:09 -0700

    airbnb models change

--- models/amenity.py
+++ models/amenity.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel, Base
-from sqlalchemy import Table, Column, String, ForeignKey
-from sqlalchemy.orm import relationship
+from models import storage_type
+from sqlalchemy import Column, String
 
 
 class Amenity(BaseModel, Base):
-    __tablename__ = "amenities"
-
-    name = Column(String(128), nullable=False)
+    '''amenity class'''
+    __tablename__ = 'amenities'
+    if storage_type == 'db':
+        name = Column(String(128), nullable=False)
+    else:
+        name = ""
